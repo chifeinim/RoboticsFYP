0f7eb2cc102a41aee3c740e3765bba0ff22ae7cb
chifeinim <chifeinim@gmail.com>
2023-11-22 17:45:54 +0000

    calibrated odometry and set travel times for forward motion and rotation

--- project.py
+++ project.py
@@ -16,8 +16,8 @@ leftMotor = BP.PORT_B
 pi = math.pi
 wheel_radius = 2.8 # cm
 wheel_distance = 14.25 # cm
-floor_modifier_move = 1.0 # 1.0 = hard floor, ? = carpet
-floor_modifier_rotate = 1.1 # 1.1 = hard floor, ? = carpet
+floor_modifier_move = 1.02 # 1.02 = hard floor, ? = carpet
+floor_modifier_rotate = 1.08 # 1.08 = hard floor, ? = carpet
 
 # length: cm
 def moveStraight(length):
@@ -25,21 +25,25 @@ def moveStraight(length):
 		length = length * floor_modifier_move
 		backwards = length < 0
 		degrees = 180 * length / (pi * wheel_radius)
+		time_delta = length / 15
 
 		BP.offset_motor_encoder(rightMotor, BP.get_motor_encoder(rightMotor))
 		BP.offset_motor_encoder(leftMotor, BP.get_motor_encoder(leftMotor))
 		BP.set_motor_limits(rightMotor, 70, 360)
 		BP.set_motor_limits(leftMotor, 70, 360)
+		current_time = time.time()
+		end_time = current_time + time_delta
 		BP.set_motor_position(rightMotor, degrees)
 		BP.set_motor_position(leftMotor, degrees)
 
-		if not backwards:
-			while (BP.get_motor_encoder(rightMotor) > int(degrees + 2) and BP.get_motor_encoder(leftMotor) > int(degrees + 2)):
-				continue
+		while (time.time() <= end_time):
+			if not backwards:
+				while (BP.get_motor_encoder(rightMotor) > int(degrees + 2) and BP.get_motor_encoder(leftMotor) > int(degrees + 2)):
+					continue
 
-		else:
-			while (BP.get_motor_encoder(rightMotor) < int(degrees - 2) and BP.get_motor_encoder(leftMotor) < int(degrees - 2)):
-				continue
+			else:
+				while (BP.get_motor_encoder(rightMotor) < int(degrees - 2) and BP.get_motor_encoder(leftMotor) < int(degrees - 2)):
+					continue
 	except IOError as error:
 		print(error)
 
@@ -47,7 +51,7 @@ def moveStraight(length):
 def rotateAntiClockwise(angle):
 	try:
 		angle = angle * floor_modifier_rotate
-		time_delta = 3
+		time_delta = angle / 90
 		angular_velocity = (1 / time_delta) * angle * wheel_distance / (2 * wheel_radius)
 
 		BP.offset_motor_encoder(rightMotor, BP.get_motor_encoder(rightMotor))
@@ -63,13 +67,20 @@ def rotateAntiClockwise(angle):
 			continue
 		BP.set_motor_dps(rightMotor, 0)
 		BP.set_motor_dps(leftMotor, 0)
+		time.sleep(0.1)
 
 	except IOError as error:
 		print(error)
 
 try:
-	#moveStraight(30)
-	rotateAntiClockwise(180)
+	moveStraight(40)
+	rotateAntiClockwise(90)
+	moveStraight(50)
+	rotateAntiClockwise(90)
+	moveStraight(50)
+	rotateAntiClockwise(90)
+	moveStraight(50)
+	rotateAntiClockwise(90)
 	#BP.reset_all()
 except KeyboardInterrupt:
 	BP.reset_all()
