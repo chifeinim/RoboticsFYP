ccd3e24450f7fce49acaabb1da2dcc8a47087b42
Chi Fei Nim <chifeinim@gmail.com>
2024-04-19 11:35:59 +0100

    implemented MCL broken

--- project.py
+++ project.py
@@ -214,7 +214,7 @@ def dynamicWindowApproach():
 		theta_start = 0.0
 		velocity_l_start = 0.0
 		velocity_r_start = 0.0
-		delta_time = 0.08
+		delta_time = 0.1
 		obstacles = []
 		starttime = time.time()
 
@@ -442,10 +442,303 @@ def calculateHomography():
 	print ("Homography")
 	print (HInv)
 
+########## MCL BEGINS ##########
+################################
+################################
 
+# A Canvas class for drawing a map and particles:
+#     - it takes care of a proper scaling and coordinate transformation between
+#      the map frame of reference (in cm) and the display (in pixels)
+class Canvas:
+	def __init__(self,map_size=210):
+		self.map_size    = map_size    # in cm;
+		self.canvas_size = 768;         # in pixels;
+		self.margin      = 0.05*map_size
+		self.scale       = self.canvas_size/(map_size+2*self.margin)
 
-dynamicWindowApproach()
-#BP.reset_all()
+	def drawLine(self,line):
+		x1 = self.__screenX(line[0])
+		y1 = self.__screenY(line[1])
+		x2 = self.__screenX(line[2])
+		y2 = self.__screenY(line[3])
+		print("drawLine:" + str((x1,y1,x2,y2)))
+
+	def drawParticles(self,data):
+		display = [(self.__screenX(d[0]),self.__screenY(d[1])) + d[2:] for d in data]
+		#print("drawParticles:" + str(display))
+
+	def __screenX(self,x):
+		return (x + self.margin)*self.scale
+
+	def __screenY(self,y):
+		return (self.map_size + self.margin - y)*self.scale
+
+# A Map class containing walls
+class Map:
+	def __init__(self):
+		self.walls = []
+
+	def add_wall(self,wall):
+		self.walls.append(wall)
+
+	def clear(self):
+		self.walls = []
+
+	def draw(self):
+		for wall in self.walls:
+			canvas.drawLine(wall)
+
+# Simple Particles set
+class Particles:
+	def __init__(self, N=100):
+		self.n = N
+		self.data = np.zeros((N,4))
+		self.data[:,3] = 1 / N
+
+	def initialise(self, x, y, theta):
+		self.data = np.zeros((self.n, 4))
+
+		ex = np.random.normal(0, 0.2, self.n)
+		ey = np.random.normal(0, 0.2, self.n)
+		f = np.random.normal(0, 0.2, self.n)
+
+		for k in range(self.n):
+			self.data[k][0] = x + ex[k]
+			self.data[k][1] = y + ey[k]
+			self.data[k][2] = theta + f[k]
+
+		#self.data[:, 0] = x
+		#self.data[:, 1] = y
+		#self.data[:, 2] = theta
+		self.data[:, 3] = 1 / self.n
+
+	def move(self, velocity_l, velocity_r, delta_time):
+		if velocity_l == velocity_r:
+			straight_sd = 0.05 # for every 1cm
+			straight_angle_sd = 0.025 # for every 1cm
+			e = np.random.normal(0, straight_sd * delta_time * velocity_l, self.n)
+			f = np.random.normal(0, straight_angle_sd * delta_time * velocity_l, self.n)
+			for k in range(self.n):
+				self.data[k][0] += (velocity_l * delta_time + e[k]) * math.cos(self.data[k][2])
+				self.data[k][1] += (velocity_l * delta_time + e[k]) * math.sin(self.data[k][2])
+				self.data[k][2] += f[k]
+
+		elif velocity_l == -velocity_r:
+			rotation_angle_sd = 1/45 * (pi / 180) # for every 1 radian, or 1/45 degrees per degree
+			g = np.random.normal(0, rotation_angle_sd * abs(((velocity_r - velocity_l) * delta_time / wheel_distance)), self.n)
+			for k in range(self.n):
+				self.data[k][2] += ((velocity_r - velocity_l * delta_time / wheel_distance) + g[k])
+
+		else:
+			arc_radius_sd = 0.03 # for every 1cm
+			arc_radius = wheel_distance / 2 * (velocity_l + velocity_r) / (velocity_r - velocity_l)
+			delta_theta_sd = 0.02 * pi / 180 # for every 1 radian
+			delta_theta = (velocity_r - velocity_l) * delta_time / wheel_distance
+			h = np.random.normal(0, abs(arc_radius_sd * arc_radius), self.n)
+			i = np.random.normal(0, abs(delta_theta_sd * delta_theta), self.n)
+			for k in range(self.n):
+				self.data[k][0] += (arc_radius + h[k]) * (math.sin(delta_theta + i[k] + self.data[k][2]) - math.sin(self.data[k][2]))
+				self.data[k][1] -= (arc_radius + h[k]) * (math.cos(delta_theta + i[k] + self.data[k][2]) - math.cos(self.data[k][2]))
+				self.data[k][0] += delta_theta + i[k]
+
+	def update_weights(self, z):
+		# adjust weights
+		total_weight = 0
+		for idx, (x, y, theta, w) in enumerate(self.data):
+			prob = calculate_likelihood(x, y, theta, z)
+			#print(f"Particle: %s" % ((x,y,theta),))
+			#print(prob)
+			self.data[idx][3] = prob * w
+			total_weight += self.data[idx][3]
+		self.data[:,3] /= total_weight
+
+	def resample(self):
+		cumulative_weights = self.data[:,3]
+		total = 0
+		new_particles = np.zeros((self.n, 4))
+		for i in range(self.n):
+			total += cumulative_weights[i]
+			cumulative_weights[i] = total
+		for i in range(self.n):
+			rand = random.random()
+			idx = 0
+			while cumulative_weights[idx] < rand and idx < self.n - 1:
+				idx += 1
+			new_particle = self.data[idx]
+			new_particle[3] = 1 / self.n
+			new_particles[i] = new_particle
+
+		self.data = new_particles
+
+	def mean(self):
+		return np.mean(self.data, axis=0)[:3]
+
+	def draw(self):
+		tupleParticles = list(map(tuple, self.data.reshape((self.n, 4))))
+		canvas.drawParticles(tupleParticles)
+
+def calculate_likelihood(x, y, theta, z):
+	K = 0.0001
+	if not z:
+		likelihood = K
+	else:
+		expected_distances = [distance(x, y, x_n, y_n) for (x_n, y_n) in waymarks]
+		waymarks_detected = len(z)
+		best_score = np.inf
+		if waymarks_detected == 1:
+			for dist in expected_distances:
+				difference = abs((distance(z[0][0], z[0][1], x, y) - dist))
+				if difference < best_score:
+					best_score = difference
+
+		elif waymarks_detected == 2:
+			for dist in expected_distances:
+				for (z1_x, z1_y) in z:
+					for (z2_x, z2_y) in z:
+						difference1 = abs((distance(z1_x, z1_y, x, y) - dist))
+						difference2 = abs((distance(z2_x, z2_y, x, y) - dist))
+						difference = difference1 * difference2
+						if difference < best_score:
+							best_score = difference
+
+		sd = 1
+		likelihood = (math.e ** ((-(best_score) ** 2) / (2 * sd ** 2))) + K
+	return likelihood
+
+waymarks  = np.array([(20,10), (70, 10), (60, 40), (30, 40)])
+
+def monteCarloLocalisation():
+	try:
+		waypoints = np.array([(90, 0), (90, 50), (0, 50), (0, 0)])
+
+		picam2 = Picamera2()
+		preview_config = picam2.create_preview_configuration(main={"size": (640, 480)})
+#		preview_config = picam2.create_preview_configuration()
+
+		picam2.configure(preview_config)
+		picam2.start()
+		white = (255,255,255)
+
+		x_start = 0.0
+		y_start = 0.0
+		theta_start = 0.0
+		velocity_l_start = 0.0
+		velocity_r_start = 0.0
+		delta_time = 0.1
+		obstacles = []
+		starttime = time.time()
+
+		for (x_target, y_target) in waypoints:
+
+			while True:
+				if distance(x_start, y_start, x_target, y_target) < 1:
+					break
+
+				z = []
+				best_cost_benefit = -10000.0
+				start_calc = time.time()
+
+				img = picam2.capture_array()
+				print("\n Captured image at time", start_calc - starttime)
+
+				img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+				hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+				# lower mask (0-10)
+				lower_red = np.array([0,50,50])
+				upper_red = np.array([10,255,255])
+				mask0 = cv2.inRange(hsv, lower_red, upper_red)
+
+				# upper mask (170-180)
+				lower_red = np.array([170,50,50])
+				upper_red = np.array([180,255,255])
+				mask1 = cv2.inRange(hsv, lower_red, upper_red)
+
+				# join my masks
+				mask = mask0+mask1
+				result = cv2.bitwise_and(img, img, mask=mask)
+
+				output = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32F)
+				(numLabels, labels, stats, centroids) = output
+
+				# loop over the number of unique connected component labels
+				for i in range(0, numLabels):
+					# i=0 is the background region
+					if i != 0:
+						# extract the connected component statistics and centroid
+						x = stats[i, cv2.CC_STAT_LEFT]
+						y = stats[i, cv2.CC_STAT_TOP]
+						w = stats[i, cv2.CC_STAT_WIDTH]
+						h = stats[i, cv2.CC_STAT_HEIGHT]
+						area = stats[i, cv2.CC_STAT_AREA]
+						(cX, cY) = centroids[i]
+						if (area > 50):
+							#print("Component", i, "area", area, "Centroid", cX, cY)
+							#img = cv2.circle(img, (int(cX), int(cY)), 5, white, 3)
+							adjustedPixels = np.array([cX, cY, 1])
+							relativePosition = predictCoordinates(adjustedPixels)
+							relative_x = relativePosition[0]
+							relative_y = relativePosition[1]
+							(z_x, z_y) = getTruePosition(x_start, y_start, theta_start, relative_x, relative_y)
+							z.append((z_x, z_y))
+
+				end_calc = time.time()
+				calc_time = end_calc - start_calc
+				possible_velocities_l = [velocity_l_start - max_acceleration * delta_time, velocity_l_start, velocity_l_start + max_acceleration * delta_time]
+				possible_velocities_r = [velocity_r_start - max_acceleration * delta_time, velocity_r_start, velocity_r_start + max_acceleration * delta_time]
+
+				for velocity_l in possible_velocities_l:
+					for velocity_r in possible_velocities_r:
+						if velocity_l <= max_velocity and velocity_r <= max_velocity and velocity_l >= -max_velocity and velocity_r >= -max_velocity:
+							(x_new, y_new, theta_new) = predictMovement(velocity_l, velocity_r, x_start, y_start, theta_start, 10 * delta_time)
+							(x_obstacle, y_obstacle) = getClosestObstacle(x_new, y_new, obstacles)
+							cost_benefit = costBenefit(x_start, y_start, x_new, y_new, x_obstacle, y_obstacle, x_target, y_target)
+							if cost_benefit > best_cost_benefit:
+								velocity_l_chosen = velocity_l
+								velocity_r_chosen = velocity_r
+								best_cost_benefit = cost_benefit
+
+				BP.set_motor_dps(leftMotor, (velocity_l_chosen / wheel_radius) * (180 / pi))
+				BP.set_motor_dps(rightMotor, (velocity_r_chosen / wheel_radius) * (180 / pi))
+				print("vl: " + str(velocity_l_chosen) + ", vr: " + str(velocity_r_chosen))
+				# New Particles code:
+				particles.move(velocity_l_chosen, velocity_r_chosen, delta_time)
+				print("z: ", z)
+				particles.update_weights(z)
+				print("mean")
+				particles.resample()
+				print(particles.mean())
+				particles.draw()
+
+				velocity_l_start = velocity_l_chosen
+				velocity_r_start = velocity_r_chosen
+				x_start, y_start, theta_start = particles.mean()
+				end_calc = time.time()
+				calc_time = end_calc - start_calc
+				print("Calculation time: ", calc_time)
+				if (start_calc - starttime > 0.3):
+					if delta_time > calc_time:
+						time.sleep(delta_time - calc_time)
+				else:
+					time.sleep(delta_time)
+
+	except KeyboardInterrupt:
+		BP.reset_all()
+		picam2.stop()
+		cv2.destroyAllWindows()
+
+canvas = Canvas()
+my_map = Map()
+my_map.add_wall((20,10,70,10))
+my_map.add_wall((70,10,60,40))
+my_map.add_wall((60,40,30,40))
+my_map.add_wall((30,40,20,10))
+my_map.draw()
+particles = Particles()
+monteCarloLocalisation()
+#dynamicWindowApproach()
+
+BP.reset_all()
 
 """
 try:
